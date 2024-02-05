1f4ebe05c1c8027771d4764d231ccdac38d2c1d9
sweana <sweana@bititude.com>
2024-02-05 11:33:37 +0530

    Add validation for create and update book

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const route = express.Router();
+const {body, validationResult} = require('express-validator')
 
 const {homePage,displayBook,displayBooks,createBook,modifyBook,removeBook} = require('../controllers/bookController')
 
@@ -9,9 +10,27 @@ route.get('/books',displayBooks);
 
 route.get('/book/:id',displayBook);
 
-route.post('/book',createBook);
+route.post('/book',[
+    body('title').isEmpty().withMessage('Title should not be empty'),
+    body('genre').isEmpty().withMessage('Genre should not be empty'),
+    body('price').isNumeric().withMessage('Price should be a number')
+],async (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.json({errors:errors.array()})
+    }
+    createBook
+});
 
-route.put('/book/:id',modifyBook);
+route.put('/book/:id',[
+    body('price').isNumeric().withMessage('Price should be a number')
+],async(req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.json({errors:errors.array()})
+    }
+    modifyBook
+});
 
 route.delete('/book/:id',removeBook);
 
